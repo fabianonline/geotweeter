830406fa455dcb38892a1bcafb5217a9ada1d2c1
Fabian Schlenz <mail@fabianonline.de>
2011-06-05 21:19:54 +0200

    Noch eine kleine Änderung, um das Reload-Problem in den Griff zu kommen. Ich bin aber weiterhin völlig ratlos.

--- geotweeter.js
+++ geotweeter.js
@@ -627,7 +627,7 @@ function replies_close() {
 function sendTweet(event) {
     if (event) event.preventDefault();
     if(error_if_no_place_set && document.tweet_form.place.options[0].selected && !confirm('Kein Ort gesetzt. Wirklich ohne Koordinaten tweeten?'))
-        return false;
+        return;
 
     var text = $('#text').val();
     
