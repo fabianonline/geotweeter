15dabfc59f963a8d86b1f952ab37095d69258a36
Jonas Sell <jonas_sell@gmx.net>
2011-08-28 17:17:45 +0200

    Jetzt mit mehr Farbe

--- geotweeter.js
+++ geotweeter.js
@@ -49,7 +49,7 @@ var followers_ids = new Array();
 var autocompletes = new Array();
 
 /** Expected version of settings.js. Gets compared to settings.version by checkSettings(). */
-var expected_settings_version = 8;
+var expected_settings_version = 9;
 
 /** Time of the last press of Enter. Used for double-Enter-recognition. */
 var timeOfLastEnter = 0;
