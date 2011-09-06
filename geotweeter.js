c414f4ffd3cd82d519804ce52eb72988a7127553
Fabian Schlenz <mail@fabianonline.de>
2011-09-06 22:45:07 +0200

    Fehlauslösungen der Vervollständigung korrigiert.
    Kam in einem Tweet *irgendwo* ein 'D' gefolgt von einem Leerzeichen vor, wurde jedes folgende
    Wort durch die die Vervollständigung zu ergänzen versucht. Gefixt.

--- geotweeter.js
+++ geotweeter.js
@@ -139,7 +139,7 @@ function start() {
         minLength: 1,
         source: function(request, response) {
             var word = extractLast(request.term);
-            if (request.term.match(/d @?[a-z0-9_]+/i)) word='@'+word;
+            if (request.term.match(/^d @?[a-z0-9_]+$/i)) word='@'+word;
             if (word[0]!="@" && word[0]!="#") response(new Array());
             else response($.ui.autocomplete.filter(autocompletes, word));
         },
