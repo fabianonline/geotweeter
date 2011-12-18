bf6cc89aec8287591a4b681fa3f3bf91ab346c3b
Fabian Schlenz <mail@fabianonline.de>
2011-12-18 01:02:20 +0100

    Weiter ausgebaut. Immer noch viel zu tun, aber langsam ernhr sich das Eichhrrrrrrnchen...

--- geotweeter.objects.coffee
+++ geotweeter.objects.coffee
@@ -1,24 +1,50 @@
 class Sender
 	constructor: (@data) ->
-	get_avatar_html: ->
-		"<span class='avatar'><img class='user_avatar' src='#{@profile_image_url}' /></span>"
-	get_link_html: ->
-		"<span class='poster'><a href='https://twitter.com/#{@screen_name}' target='_blank'>#{@screen_name}</a></span>"
+	get_avatar_html: -> "<span class='avatar'><img class='user_avatar' src='#{@profile_image_url}' /></span>"
+	get_link_html: -> "<span class='poster'><a href='https://twitter.com/#{@screen_name}' target='_blank'>#{@screen_name}</a></span>"
 
 class TwitterMessage
-	constructor: (@data) ->
-		@sender = new Sender(@data.sender)
+	constructor: (@data) -> @sender = new Sender(@data.sender)
 	
 class window.Tweet extends TwitterMessage
+	@mentions = []
+	
 	constructor: (data) ->
 		super(data)
-		@sender = new Sender(@data.user)
+		@sender = new Sender(data.user)
 		window.tweets[@id()] = this
-
+		@text = data.status
+		@linkify()
+		
 	id: -> @data.id_str
 	div_id: -> "#tweet_#{@id()}"
 	get_html: ->
 		"<div id='#{@id()}'>" +
 		@sender.get_avatar_html() +
 		@sender.get_link_html() +
+		@text +
 		"</div>"
+	
+	linkify: ->
+		if @data.entities?
+			all_entities = []
+			for entity_type, entities of @data.entities
+				for entity in entities
+					entity.type=entity_type
+					all_entities.push(entity)
+			all_entities = all_entities.sort((a, b) -> a.indices[0] - b.indices[0]).reverse()
+			for entity in all_entities
+				switch entity.type
+					when "user_mentions"
+						@mentions.push entity.screen_name
+						@replace_entity(entity, "<a href='https://twitter.com/#{entity.screen_name}' target='_blank'>@#{entity.screen_name}</a>")
+					when "urls", "media"
+						if entity.expanded_url?
+							@replace_entity(entity, "<a href='#{entity.expanded_url}' class='external' target='_blank'>#{entity.display_url}</a>")
+						else
+							@replace_entity(entity, "<a href='#{entity.url}' class='external' target='_blank'>#{entity.url}</a>")
+					when "hashtags"
+						@replace_entity(entity, "<a href='https://twitter.com/search?q=##{entity.text}' target='_blank'>##{entity.text}</a>")
+						
+	
+	replace_entity: (entity_object, text) -> @text = @text.slice(0, entity_object.indices[0]) + text + @text.slice(entity_object.indices[1])
--- geotweeter.objects.js
+++ geotweeter.objects.js
@@ -35,10 +35,14 @@
 
     __extends(Tweet, TwitterMessage);
 
+    Tweet.mentions = [];
+
     function Tweet(data) {
       Tweet.__super__.constructor.call(this, data);
-      this.sender = new Sender(this.data.user);
+      this.sender = new Sender(data.user);
       window.tweets[this.id()] = this;
+      this.text = data.status;
+      this.linkify();
     }
 
     Tweet.prototype.id = function() {
@@ -50,7 +54,54 @@
     };
 
     Tweet.prototype.get_html = function() {
-      return ("<div id='" + (this.id()) + "'>") + this.sender.get_avatar_html() + this.sender.get_link_html() + "</div>";
+      return ("<div id='" + (this.id()) + "'>") + this.sender.get_avatar_html() + this.sender.get_link_html() + this.text + "</div>";
+    };
+
+    Tweet.prototype.linkify = function() {
+      var all_entities, entities, entity, entity_type, _i, _j, _len, _len2, _ref, _results;
+      if (this.data.entities != null) {
+        all_entities = [];
+        _ref = this.data.entities;
+        for (entity_type in _ref) {
+          entities = _ref[entity_type];
+          for (_i = 0, _len = entities.length; _i < _len; _i++) {
+            entity = entities[_i];
+            entity.type = entity_type;
+            all_entities.push(entity);
+          }
+        }
+        all_entities = all_entities.sort(function(a, b) {
+          return a.indices[0] - b.indices[0];
+        }).reverse();
+        _results = [];
+        for (_j = 0, _len2 = all_entities.length; _j < _len2; _j++) {
+          entity = all_entities[_j];
+          switch (entity.type) {
+            case "user_mentions":
+              this.mentions.push(entity.screen_name);
+              _results.push(this.replace_entity(entity, "<a href='https://twitter.com/" + entity.screen_name + "' target='_blank'>@" + entity.screen_name + "</a>"));
+              break;
+            case "urls":
+            case "media":
+              if (entity.expanded_url != null) {
+                _results.push(this.replace_entity(entity, "<a href='" + entity.expanded_url + "' class='external' target='_blank'>" + entity.display_url + "</a>"));
+              } else {
+                _results.push(this.replace_entity(entity, "<a href='" + entity.url + "' class='external' target='_blank'>" + entity.url + "</a>"));
+              }
+              break;
+            case "hashtags":
+              _results.push(this.replace_entity(entity, "<a href='https://twitter.com/search?q=#" + entity.text + "' target='_blank'>#" + entity.text + "</a>"));
+              break;
+            default:
+              _results.push(void 0);
+          }
+        }
+        return _results;
+      }
+    };
+
+    Tweet.prototype.replace_entity = function(entity_object, text) {
+      return this.text = this.text.slice(0, entity_object.indices[0]) + text + this.text.slice(entity_object.indices[1]);
     };
 
     return Tweet;
