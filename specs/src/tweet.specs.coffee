a4c1323dbfdc3000e14a2e89b16b30d4995e0e26
Fabian Schlenz <mail@fabianonline.de>
2012-01-25 15:19:21 +0100

    Mehr Tests.

--- specs/src/tweet.specs.coffee
+++ specs/src/tweet.specs.coffee
@@ -12,9 +12,24 @@ describe('Normal Tweet', ->
 		expect(tweet.id).toEqual("162157777156980736")
 	)
 
-	describe('classes', ->
-		it('should have some', ->
-			expect(tweet.get_classes()).toContain("tweet")
-		)
+	it('should have some classes', ->
+		expect(tweet.get_classes()).toContain("tweet")
+		expect(tweet.get_classes()).toContain("by_zurvollenstunde")
+		expect(tweet.get_classes()).toContain("new")
+		expect(tweet.get_classes()).not.toContain("by_this_user")
+		expect(tweet.get_classes()).not.toContain("mentions_this_user")
+	)
+	
+	it('should fill it\'s user variable', ->
+		expect(tweet.sender.constructor).toEqual(User)
+	)
+	
+	it("should not have a retweeted_by user", ->
+		expect(tweet.retweeted_by).toBeNull()
+	)
+	
+	it("should have no thumbnails", ->
+		expect(tweet.get_single_thumb_html()).toEqual("")
+		expect(tweet.get_multi_thumb_html()).toEqual("")
 	)
 )
--- specs/tweet.specs.js
+++ specs/tweet.specs.js
@@ -11,9 +11,21 @@ describe('Normal Tweet', function() {
   it('should have an id', function() {
     return expect(tweet.id).toEqual("162157777156980736");
   });
-  return describe('classes', function() {
-    return it('should have some', function() {
-      return expect(tweet.get_classes()).toContain("tweet");
-    });
+  it('should have some classes', function() {
+    expect(tweet.get_classes()).toContain("tweet");
+    expect(tweet.get_classes()).toContain("by_zurvollenstunde");
+    expect(tweet.get_classes()).toContain("new");
+    expect(tweet.get_classes()).not.toContain("by_this_user");
+    return expect(tweet.get_classes()).not.toContain("mentions_this_user");
+  });
+  it('should fill it\'s user variable', function() {
+    return expect(tweet.sender.constructor).toEqual(User);
+  });
+  it("should not have a retweeted_by user", function() {
+    return expect(tweet.retweeted_by).toBeNull();
+  });
+  return it("should have no thumbnails", function() {
+    expect(tweet.get_single_thumb_html()).toEqual("");
+    return expect(tweet.get_multi_thumb_html()).toEqual("");
   });
 });
