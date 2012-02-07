2d6adb984d16add08a97358e5f621e5582d1cd3c
Fabian Schlenz <mail@fabianonline.de>
2012-02-07 18:16:48 +0100

    Man kann jetzt auch './build.sh specs --watch' aufrufen.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ "$1" = "specs" ]; then
-	coffee --bare --output specs/ --compile specs/src/*.coffee
+	coffee $2 --bare --output specs/ --compile specs/src/*.coffee
 else
 	coffee $@ --bare --join geotweeter.js --compile \
 		src/_info.coffee \
